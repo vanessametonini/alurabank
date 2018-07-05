8bda0d90656940438e5bbdece9e28bcd56c81f45
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-07-05 17:09:22 -0300

    JS Puro e seus problemas com classes

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,36 @@
+class Negociacao {
+    constructor(data, quantidade, valor){
+
+        if(!data){
+            throw Error('Uma data precisa ser definida')
+        }
+
+        if(!quantidade){
+            throw Error('Uma quantidade precisa ser definida')
+        }
+
+        if(!valor){
+            throw Error('Um valor precisa ser definida')
+        }
+        
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data(){
+        return this._data
+    }
+
+    get quantidade(){
+        return this._quantidade
+    }
+
+    get valor(){
+        return this._valor
+    }
+
+    get volume(){
+        return this._valor * this._quantidade
+    }
+}
