de52c039a896c7da2e3bf163afbe8c389dc43396
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-07-27 16:23:33 -0300

    Cap03. ambiente e classese negociacao

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,4 @@
-let negociacao = new Negociacao(new Date(), 2, 100)
-
-negociacao._quantidade = 34 
-
+let negociacao = new Negociacao(new Date(), 2, 100);
+negociacao._quantidade = 34;
 console.log(negociacao._quantidade);
 console.log(negociacao);
---
+++ app/ts/app.ts
@@ -0,0 +1,6 @@
+let negociacao = new Negociacao(new Date(), 2, 100)
+
+negociacao._quantidade = 34 
+
+console.log(negociacao._quantidade);
+console.log(negociacao);
